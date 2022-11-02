9b8907fbc084dd967b932949889aab22ee7192da
rheber <rheber@rediffmail.com>
2022-11-02 18:44:30 +1100

    style tab headers

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -2,8 +2,44 @@
   export let tabs = [];
 
   let activeIdx = 0;
+
+  const setActiveIdx = (newIdx) => {
+    activeIdx = newIdx;
+  };
 </script>
 
 <div>
+  <div class="headers">
+    {#each tabs as tab, idx (tab)}
+      <div
+        class="header"
+        class:inactive={idx !== activeIdx}
+        on:click="{() => setActiveIdx(idx)}"
+        on:keypress="{() => setActiveIdx(idx)}"
+      >
+        {tab}
+      </div>
+    {/each}
+  </div>
   <slot />
 </div>
+
+<style>
+  .header {
+    cursor: default;
+    text-align: center;
+    width: 100%;
+  }
+
+  .header.inactive {
+    color: gray;
+    cursor: pointer;
+  }
+
+  .headers {
+    box-shadow: 1px 1px 8px;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 8px;
+  }
+</style>
