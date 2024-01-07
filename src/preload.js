e55e49cefb248af0e54e9afeb0146c1344c8ec31
rheber <rheber@rediffmail.com>
2024-01-07 16:49:37 +1100

    obey xdg spec

--- src/preload.js
+++ src/preload.js
@@ -2,8 +2,15 @@ const fsPromises = require('fs/promises');
 const untildify = require('untildify');
 const { contextBridge } = require('electron');
 
+// Calculate path to file that meets the XDG Base Directory specification.
+const xdgCachePath = () => {
+    const homeDir = process.env.HOME || untildify('~');
+    const dataDir = process.env.XDG_DATA_HOME || homeDir;
+    return `${dataDir}/.ghx.cache`;
+}
+
 const loadCache = async () => {
-    const cachePath = untildify('~/.ghx.cache');
+    const cachePath = xdgCachePath();
     try {
         const data = await fsPromises.readFile(cachePath, 'utf-8');
         return data;
@@ -19,7 +26,7 @@ const loadCache = async () => {
 };
 
 const saveCache = async (data) => {
-    const cachePath = untildify('~/.ghx.cache');
+    const cachePath = xdgCachePath();
     try {
         const serialized = JSON.stringify(data);
         await fsPromises.writeFile(cachePath, serialized);
